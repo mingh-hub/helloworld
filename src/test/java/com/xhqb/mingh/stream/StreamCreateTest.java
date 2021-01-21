b0fbbf8ce92bbfae7e688bac1895dc7d483d0aee
minghai <minghai@xhqb.com>
2021-01-21 20:40:29 +0800

    stream create style test

--- src/test/java/com/xhqb/mingh/stream/StreamCreateTest.java
+++ src/test/java/com/xhqb/mingh/stream/StreamCreateTest.java
@@ -1,14 +1,12 @@
 package com.xhqb.mingh.stream;
 
-import com.xhqb.mingh.beans.Address;
-import com.xhqb.mingh.beans.User;
-import com.xhqb.mingh.common.enums.ResultEnum;
-import com.xhqb.mingh.common.exception.BusinessRuntimeException;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections4.CollectionUtils;
 import org.junit.Test;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
@@ -24,13 +22,26 @@ public class StreamCreateTest {
      */
     @Test
     public void testCreateStreamWithStreamStaticMethod() {
-        User user = User.builder().build();
-        String province = Optional.ofNullable(user)
-                .filter(u -> CollectionUtils.isNotEmpty(u.getAddress()) && Objects.nonNull(u.getAddress().get(0)))
-                .map(u -> u.getAddress().get(0))
-                .map(Address::getProvince)
-                .orElseThrow(() -> new BusinessRuntimeException(ResultEnum.FAILURE));
-        log.info("testCreateStreamWithStreamStaticMethod province is {}", province);
+        // Stream.of()
+        Stream<Integer> integerStream = Stream.of(1, 3, 5, 7, 9);
+        List<Integer> list = integerStream.collect(Collectors.toList());
+        log.info("list is {}", list);
+
+        // Stream.iterate(), i++ 延迟计算并一直输出为 0, ++i 先计算后输出
+        List<Integer> list1 = Stream.iterate(0, i -> i++).limit(10).collect(Collectors.toList());
+        log.info("list1 is {}", list1);  // [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        List<Integer> list2 = Stream.iterate(0, i -> ++i).limit(10).collect(Collectors.toList());
+        log.info("list2 is {}", list2);  // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+        // 通过 Stream.generate 方法从外部传入一个提供元素的 Supplier 来构造无限流，然后使用 limit 限制流元素个数
+//        List<Double> doubleList = Stream.generate(Math::random).collect(Collectors.toList());  // java.lang.OutOfMemoryError: Java heap space
+        List<Double> doubleList = Stream.generate(Math::random).limit(3).collect(Collectors.toList());
+        log.info("doubleList is {}", doubleList);
+
+        // 通过 IntStream 或 DoubleStream 构造基本类型的流
+        IntStream.range(1, 5).forEach(i -> log.info(String.valueOf(i)));
+        IntStream.rangeClosed(1, 5).forEach(i -> log.info(String.valueOf(i)));
+        IntStream.rangeClosed(1, 5).mapToObj(String::valueOf).forEach(log::info);
     }
 
     /**
@@ -40,7 +51,7 @@ public class StreamCreateTest {
     public void testCreateStreamWithArray() {
         int[] array = {1, 3, 5, 7, 9};
         IntStream intStream = Arrays.stream(array);
-        intStream.forEach(i -> log.info(String.valueOf(i)));
+        intStream.mapToObj(String::valueOf).forEach(log::info);
     }
 
     /**
