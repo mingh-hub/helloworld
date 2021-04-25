9ca0cb506d9d3a4feabaeaa883052f4a199d0080
minghai <minghai@xhqb.com>
2021-04-25 10:08:04 +0800

    代码微调

--- src/main/java/com/mingh/learn/HelloWorld.java
+++ src/main/java/com/mingh/learn/HelloWorld.java
@@ -1,19 +1,25 @@
 package com.mingh.learn;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.google.common.collect.Lists;
+import com.mingh.learn.beans.Book;
 import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
 
 public class HelloWorld {
 
     public static void main(String[] args) {
-        try {
-            int a = 4 / 0;
-        } catch (ClassCastException e) {
-            System.out.println(e);
-        }
-        finally {
+        Book book = Book.builder().bookName("test01").build();
+        List<Book> bookList = Lists.newArrayList();
+        bookList.add(book);
+        String jsonData = JSON.toJSONString(bookList);
+        System.out.println(jsonData);
 
-            System.out.println("==================");
-        }
+        List<Book> list = JSONArray.parseArray(jsonData, Book.class);
+        System.out.println(list);
+
     }
 
     private static String filterAddressUnsupportChar(String address) {
--- src/main/java/com/mingh/learn/beans/Book.java
+++ src/main/java/com/mingh/learn/beans/Book.java
@@ -1,10 +1,14 @@
 package com.mingh.learn.beans;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book extends BaseBean{
     private static final long serialVersionUID = -5966361323385755069L;
 
