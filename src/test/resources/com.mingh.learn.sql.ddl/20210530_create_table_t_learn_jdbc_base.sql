a322ea0b9fd4427a483045400a1a18ece2d62352
minghai <minghai@xhqb.com>
2021-05-30 00:31:00 +0800

    sql update, no need to pay close attention to this

--- src/test/resources/com.mingh.learn.sql.ddl/20210530_create_table_t_learn_jdbc_base.sql
+++ src/test/resources/com.mingh.learn.sql.ddl/20210530_create_table_t_learn_jdbc_base.sql
@@ -8,4 +8,4 @@ create table if not exists `learn`.`t_learn_jdbc_base`
     description varchar(500) default null comment '说明',
     create_time datetime null comment '创建时间',
     update_time datetime null comment '修改时间'
-    ) comment 'created for learning jdbc' ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) comment 'created for learning jdbc' ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
